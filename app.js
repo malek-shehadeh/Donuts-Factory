cc1f7dc878594ce2b41585441056ada8a17ba99c
malekshehadeh <malek.shehadeh96@gmail.com>
2024-06-04 23:02:41 +0300

    firstversion

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-let FullName = prompt("Enter Your Name : ");
-let gender = prompt("Enter Your Gender : ");
-if (gender == "male") {
-  alert("Welcome MR." + FullName);
-} else if (gender == "female") {
-  alert("Welcome MS." + FullName);
-} else {
-  alert("Welcome " + FullName);
-}
-
-let Q1 = confirm("Do you want buy donuts or coffee or ice cream or bakery ?");
-if (Q1 == true) {
-  var order = prompt("write the order want to buy ?");
-  alert("The order will prepare ");
-}
-console.log("My name " + FullName + " " + "order is " + order);
+// let FullName = prompt("Enter Your Name : ");
+// let gender = prompt("Enter Your Gender : ");
+// if (gender == "male") {
+//   alert("Welcome MR." + FullName);
+// } else if (gender == "female") {
+//   alert("Welcome MS." + FullName);
+// } else {
+//   while (gender != "male" || gender != "female") {
+//     gender = prompt("Enter Your Gender : ");
+//     if (gender == "male") {
+//       alert("Welcome MR." + FullName);
+//       break;
+//     } else if (gender == "female") {
+//       alert("Welcome MS." + FullName);
+//       break;
+//     }
+//   }
+// }
+// let Q1 = confirm("Do you want buy donuts or coffee or ice cream or bakery ?");
+// if (Q1 == true) {
+//   var order = prompt("write the order want to buy ?");
+//   alert("The order will prepare ");
+// } else {
+//   alert("thanks");
+// }
+// console.log("My name " + FullName + " " + "order is " + order);
+// let allorder = [FullName, gender, Q1, order];
+// for (let index = 0; index < allorder.length; index++) {
+//   console.log(allorder[index]);
+// }
